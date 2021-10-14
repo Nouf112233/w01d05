4697e6e072ec6684a303f88b92db6758eb138f18
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-10-14 12:27:07 +0300

    add second test

---
+++ test2/dataTypes.js
@@ -0,0 +1,35 @@
+let age = 33;
+let namee = "Nouf";
+let info = ['w1','d5','javascript',1];
+let course={
+    theWeek: 1,
+    theDay: 5,
+    supject : 'javaScript'
+};
+console.log(age);
+console.log(namee);
+console.log(info);
+console.log(course);
+
+let ageType = typeof age;
+let nameeType = typeof namee;
+let infoType = typeof info;
+let courseType = typeof course;
+
+
+console.log(`the type of ${age} is ${ageType}`);
+console.log(`the type of ${namee} is ${nameeType}`);
+console.log(`the type of ${info} is ${infoType}`);
+console.log(`the type of ${course} is ${courseType}`);
+
+
+let arr =[1,'nouf' ,49.6,'alateeq'];
+let arrType =typeof arr;
+console.log(arrType),
+
+
+let x = null;
+let indVar ;
+
+
+
