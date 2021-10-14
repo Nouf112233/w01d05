e7509eb81990755c0e44a8c77da9fc80d82863a7
Nouf112233 <Nouf.Mohamed.elq.js@tuwaiq.edu.sa>
2021-10-14 11:39:47 +0300

    add first task

---
+++ test1/myVariables.js
@@ -0,0 +1,16 @@
+let language = "JavaScript";
+let score = "10";
+console.log(`${language} ${score}`);
+
+let length = 20;
+let width = 5;
+let area = length* width;
+console.log(`The area of the rectangle is ${area}`);
+
+let num1 = 100;
+let num2 = 45;
+let rem = num1%num2;
+let div = (num1/num2)-rem;
+console.log(`The remainder of divided ${num1} / ${num2} is ${rem}`);
+console.log(`${num1} / ${num2} = ${div}  remainder ${rem}`);
+
